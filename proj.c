1d489121bb0d51f699d7b8751ea93010d670f3b7
Tim Regan <timregan10@gmail.com>
2017-05-06 17:01:01 -0500

    added sbit for buttons and lights

--- proj.c
+++ proj.c
@@ -1,5 +1,27 @@
 #include <reg932.h>
 
+sbit light1 = P2^4;
+sbit light2 = P0^5;
+sbit light3 = P2^7;
+sbit light4 = P0^6;
+sbit light5 = P1^6;
+sbit light6 = P0^4;
+sbit light7 = P2^5;
+sbit light8 = P0^7;
+sbit light9 = P2^6;
+
+sbit button1 = P2^0;
+sbit button2 = P0^1;
+sbit button3 = P2^3;
+sbit button4 = P0^2;
+sbit button5 = P1^4;
+sbit button6 = P0^0;
+sbit button7 = P2^1;
+sbit button8 = P0^3;
+sbit button9 = P2^2;
+
+unsigned char mode = 0;
+
 void main() {
-	
+	if (
 }
