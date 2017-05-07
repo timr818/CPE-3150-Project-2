edc934c7f2009cd67e09c0d9e6170e79bc280ac7
Matthew Qualls <mqggd@mst.edu>
2017-05-07 16:18:34 -0500

    heeey

--- proj.c
+++ proj.c
@@ -1,6 +1,45 @@
 #include <reg932.h>
 #include <uart.h>
 
+#define OSC_FREQ		7372800
+
+#define C4				((OSC_FREQ/4)/261.63) 
+#define Db4				((OSC_FREQ/4)/277.18)
+#define D4				((OSC_FREQ/4)/293.67)
+#define Eb4				((OSC_FREQ/4)/311.13)
+#define E4				((OSC_FREQ/4)/329.63)
+#define F4				((OSC_FREQ/4)/349.23)
+#define Gb4				((OSC_FREQ/4)/369.99)
+#define G4				((OSC_FREQ/4)/392.00)
+#define Ab4				((OSC_FREQ/4)/415.30)
+#define A4				((OSC_FREQ/4)/440.00) 
+#define Bb4				((OSC_FREQ/4)/466.16) 
+#define B4				((OSC_FREQ/4)/493.88) 
+#define C5				((OSC_FREQ/4)/523.25) 
+#define Db5				((OSC_FREQ/4)/554.37) 
+#define D5				((OSC_FREQ/4)/587.33) 
+#define Eb5				((OSC_FREQ/4)/622.25) 
+#define E5				((OSC_FREQ/4)/659.26) 
+#define F5				((OSC_FREQ/4)/698.46) 
+#define Gb5				((OSC_FREQ/4)/739.99) 
+#define G5				((OSC_FREQ/4)/783.99) 
+#define Ab5				((OSC_FREQ/4)/830.61) 
+#define A5				((OSC_FREQ/4)/880.00) 
+#define Bb5				((OSC_FREQ/4)/932.33) 
+#define B5				((OSC_FREQ/4)/987.77) 
+#define C6				((OSC_FREQ/4)/1046.5) 
+#define Db6				((OSC_FREQ/4)/1108.7) 
+#define D6				((OSC_FREQ/4)/1174.7) 
+#define Eb6				((OSC_FREQ/4)/1244.5) 
+#define E6				((OSC_FREQ/4)/1318.5) 
+#define F6				((OSC_FREQ/4)/1396.9) 
+#define Gb6				((OSC_FREQ/4)/1480.0) 
+#define G6				((OSC_FREQ/4)/1568.0) 
+#define Ab6				((OSC_FREQ/4)/1661.2) 
+#define A6				((OSC_FREQ/4)/1760.0) 
+#define Bb6				((OSC_FREQ/4)/1864.7) 
+#define B6				((OSC_FREQ/4)/1975.5) 
+
 sbit light1 = P2^4;
 sbit light2 = P0^5;
 sbit light3 = P2^7;
@@ -32,6 +71,9 @@ unsigned char smStart;
 unsigned char smEnd;
 unsigned char rrStart;
 unsigned char rrEnd;
+unsigned int freq;
+unsigned int dur;
+unsigned char index;
 
 void delay(unsigned int time);
 void playsound(unsigned int dur, unsigned int del);
@@ -39,12 +81,29 @@ void piano(void);
 void uart_init (void);
 void uart_isr(void);
 void uart_transmit(char c);
+void sqr_wave();
+void allstar();
+void rickroll();
+
+code int SmashMouth[] = {G5, D6, B5, B5, A5, G5, G5, C6, B5, B5, A5, A5, G5, 0, G5, D6, B5, B5, A5, A5, G5, G5, E5, D5, 0};
+
+code int durSmashMouth[] = {64, 32, 32, 64, 32, 32, 32, 64, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 64, 64, 32};
+
+code int GiveYouUp[] = {G5, A5, C6, A5, E6, E6, D6, G5, A5, B5, G5, 
+											D6, D6, C6, B5, A5, G5, A5, B5, G5, C6, D6, B5,
+											A5, G5, 0, G5, D6, C6, 0};
+
+code int durGiveYouUp[] = {16, 16, 16, 16, 32, 32, 64, 16, 16, 16, 16, 32, 32, 32, 16, 32, 16, 16, 16, 16, 64, 32, 48, 16, 32, 32, 32, 64, 64, 32};
 
 char trans;
 
 void main(void) {
 	mode = 1;
-	buttonDelay = 1000;
+	buttonDelay = 15000;
+	
+	TMOD = 0x21;
+	EA = 1;
+	ET0 = 1;
 	
 	//initialize the ports so buttons and lights work
 	P2M1=0;
@@ -103,7 +162,6 @@ void main(void) {
 		//selection mode
 		if (!button8) { //mode is selected
 			if (mode == 1) {
-				light1=0;
 				uart_transmit('S');
 				uart_transmit('M');
 				uart_transmit('A');
@@ -115,7 +173,10 @@ void main(void) {
 				uart_transmit('U');
 				uart_transmit('T');
 				uart_transmit('H');
-				light1=1;
+				uart_transmit(' ');
+				
+				allstar();
+				delay(buttonDelay);
 			} else if (mode == 2) {
 				uart_transmit('R');
 				uart_transmit('I');
@@ -126,6 +187,10 @@ void main(void) {
 				uart_transmit('O');
 				uart_transmit('L');
 				uart_transmit('L');
+				uart_transmit(' ');
+				
+				rickroll();
+				delay(buttonDelay);
 			} else if (mode == 3) {
 				light2=1;
 				light3=1;
@@ -136,6 +201,7 @@ void main(void) {
 		}
 	}
 }
+
 
 void piano(void) {
 	unsigned char play = 1; //boolean for quiting the piano mode
@@ -172,6 +238,7 @@ void piano(void) {
 	}
 }
 
+
 //plays a sound for a duration and del determines pitch
 void playsound(unsigned int dur, unsigned int del) {
 	unsigned int i;
@@ -204,16 +271,19 @@ void uart_init (void) {
   AUXR1 |= 0x40;
 
   // configure baud rate generator
-  //BRGCON = 0x00;
-  //BRGR0 = 0xF0;
-  //BRGR1 = 0x02;
-  //BRGCON = 0x03;
+  
+	BRGCON = 0x00;
+  BRGR0 = 0xF0;
+  BRGR1 = 0x02;
+  BRGCON = 0x03;
+	/**/
+	/*
 	EA = 1;
 	ET1 = 1;
 	TMOD = 0x21;
-	TH1 = -3;
+	TH1 = -6;
 	TR1 = 1;
-	
+	/**/
 
   // TxD = push-pull, RxD = input
   P1M1 = 0x02;
@@ -256,4 +326,46 @@ void uart_transmit(char c) {
   while(mtxbusy);
   mtxbusy = 1;
   SBUF = c;
+}
+
+void allstar() {
+	for(index = 0; index < 25; index++) {
+		delay(1000);
+		if (SmashMouth[index] != 0) {
+			freq = SmashMouth[index];
+			dur = durSmashMouth[index] * 500;
+			TR0 = 1;
+			delay(dur);
+		} else if (SmashMouth[index] == 0) {
+			TR0 = 0;
+			dur = durSmashMouth[index];
+			delay(dur * 500);
+		} else {
+			TR0 = 0;
+		}
+	}
+}
+
+void rickroll() {
+	for(index = 0; index < 30; index++) {
+		delay(1000);
+		if (GiveYouUp[index] != 0) {
+			freq = GiveYouUp[index];
+			dur = durGiveYouUp[index] * 500;
+			TR0 = 1;
+			delay(dur);
+		} else if (GiveYouUp[index] == 0) {
+			TR0 = 0;
+			dur = durGiveYouUp[index];
+			delay(dur * 500);
+		} else {
+			TR0 = 0;
+		}
+	}
+}
+
+void sqr_wave() interrupt 1 {
+	speaker = ~speaker;
+	TH0 = -(freq)>>8;
+	TL0 = -(freq);
 }
